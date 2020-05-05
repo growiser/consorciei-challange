c26186b6f38cc7725be11347812cf3b87c7766b9
Gabriel Braga Costa <kevencript@gmail.com>
2020-05-05 17:41:02 -0300

    Documented the code a bit more

--- authMiddleware/helpers/permission.js
+++ authMiddleware/helpers/permission.js
@@ -1,13 +1,16 @@
 function setPermission(source, method, route, allowed) {
+    // Verify if user contain the necessery permissions ID's 
     const isAllowed = source.permissions.some(permission=> allowed.includes(permission))
     console.log('isALlowed', isAllowed)
     
+    // Verify if we don't change anything
     if(source.route === route && source.method === method) {
         if(isAllowed) {
+            // Generating Allowed Policy
             source.callback(null, source.generatePolicy('user', 'Allow', source.arn))
             return true
         } else {
-
+            // Generating Denied Policy
             source.callback(null, source.generatePolicy('user', 'Deny', source.arn))
         }
     } else {
