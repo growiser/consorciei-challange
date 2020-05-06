e2af856c5c41c2b1fcb3ce7f13bd0f76be52d88b
Gabriel Braga Costa <kevencript@gmail.com>
2020-05-06 19:31:09 -0300

    [FEATURE] Added new seeders

--- seeders/20200505235127-user-manager.js
+++ seeders/20200505235127-user-manager.js
@@ -1,15 +1,26 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     
 
-    return queryInterface.bulkInsert('access_groups', [
+    await queryInterface.bulkInsert('access_groups', [
       {
         static_id:1,
         group_name: "User manager"
       }], {});
   
+    await queryInterface.bulkInsert('user_profile', [
+      {
+        username: "user-manager",
+        hashed_password: "$2a$10$VyEgSdjJlNQ751wbqS9ut.GLJs1tW2ckj8y/l557a9/LMOSXk5JD6"
+      }], {});
+
+      await queryInterface.bulkInsert('user_current_groups', [
+        {
+          id_user_profile: 1, // Will be the first user of the DB
+          id_access_group: 1 
+        }], {});
   },
 
   down: (queryInterface, Sequelize) => {
