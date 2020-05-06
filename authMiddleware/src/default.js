05346fd26626c21368ca30ce7a105253dd97a231
Gabriel Braga Costa <kevencript@gmail.com>
2020-05-06 15:10:18 -0300

    [BUGFIX] Setting new defualt values

--- authMiddleware/src/default.js
+++ authMiddleware/src/default.js
@@ -1,6 +1,6 @@
 myHandler = require("../index")
 
 myHandler.handler({
-    authorizationToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZnVsbF9uYW1lIjoiSEVDVE9SIEdSRUNDTyBNT1JBRVMgQkFQVElTVEEgTU9SRUlSQSIsImFjY2Vzc19ncm91cF9pZCI6IlsxLDIsM10iLCJpYXQiOjE1NjUwMjQyODYsImV4cCI6MTU2NTAyNzg4Nn0.Hoov4-KfywJY8Mb2-ryPbaRhkGY88O90I4Oif97sEq4',
-    methodArn: 'arn:aws:execute-api:us-east-1:465723213600:j5kxnacygl/dev/POST/cms/posts'
+    authorizationToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgsImFjY2Vzc19ncm91cF9pZCI6WzEsMl0sImlhdCI6MTU4ODc4NDc5MywiZXhwIjoxNTg4Nzg4MzkzfQ.xq3tLjneWbRMD9IaLpDy_Twf8MBaGRMVkOuXcQT-6CA',
+    methodArn: 'arn:aws:lambda:us-east-2:764978587799:function:dev-consorciei-createUser'
 })
